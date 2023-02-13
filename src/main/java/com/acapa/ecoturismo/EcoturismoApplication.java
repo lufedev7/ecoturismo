4c6f84210ccb5fe59c05f257156ba06d10019c2e
lufdev <luisfer738@gmail.com>
2023-02-12 22:00:50 -0500

    Initializer project

---
+++ src/main/java/com/acapa/ecoturismo/EcoturismoApplication.java
@@ -0,0 +1,13 @@
+package com.acapa.ecoturismo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoturismoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoturismoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acapa/ecoturismo/EcoturismoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acapa.ecoturismo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoturismoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
